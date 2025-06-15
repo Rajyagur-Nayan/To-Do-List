785609533a941d2a756dd84ab625e72bf9919344
Rajyaguru Nayan <nayanrajyaguru59@gamil.com>
2025-06-16 02:11:51 +0530

    changes

--- To-Do-List/vite.config.js
+++ To-Do-List/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
   build: {
-    outDir: "build", // <-- change output folder from 'dist' to 'build'
+    outDir: "build",
   },
 });
